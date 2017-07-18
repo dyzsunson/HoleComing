6657f0ab62c21647c130a0fd62d595e3f1ae98c3
LizhouCao <lizhoucao1990@gmail.com>
2017-07-18 17:59:48 -0500

    aaa

--- HoleComing/Assets/Scripts/SceneController.cs
+++ HoleComing/Assets/Scripts/SceneController.cs
@@ -21,6 +21,20 @@ public class SceneController : MonoBehaviour {
 	}
 
     public void ChangeScore(int _total, int _check, int _hit) {
-        Score_Text.text = "" + _total + " " + _check + " " + _hit;
+        int finalScore = (_check - _hit) * 100 / _total;
+        string text = "";
+
+        if (finalScore > 90)
+            text = "Perfect";
+        else if (finalScore > 70)
+            text = "Good";
+        else if (finalScore > 50)
+            text = "OK";
+        else
+            text = "Bad";
+
+        Score_Text.text = "" + _total + " " + _check + " " + _hit + " " + finalScore + " " + text;
+
+
     }
 }
